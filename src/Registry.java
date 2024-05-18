bf7e7940e230564a706a2cc9835e8887fb1855da
Abhi-010 <115487002+Abhi-010@users.noreply.github.com>
2024-05-19 00:46:03 +0530

    first commit on master

---
+++ src/Registry.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Registry {
+    private HashMap<String,String> register ;
+
+    public Registry(HashMap<String,String> register){
+        this.register = new HashMap<>();
+    }
+
+    public void doSomething(){
+        //
+    }
+}
