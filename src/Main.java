628dfe76cc61733f694f0928176db7cd369cb816
Abhi-010 <115487002+Abhi-010@users.noreply.github.com>
2024-05-19 01:13:21 +0530

    second commit on master

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is register design patter!");
     }
+
+    public static void method1(){
+        System.out.println("A method1 is the special method in this class..");
+    }
 }
